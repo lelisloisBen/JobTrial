4248aabe7cbf5ac29165324b0575caed9d5fb433
samir benzada <samirbenzada@gmail.com>
2020-02-22 23:17:30 -0500

    -m

--- src/js/views/Login/Login.js
+++ src/js/views/Login/Login.js
@@ -4,7 +4,7 @@ import { UserContext } from '../../../UserContext';
 import styles from './Login.module.css';
 import swal from 'sweetalert';
 
-import coin from '../../../img/coin.png';
+import worker from '../../../img/worker.png';
 
 const Login = () => {
 
@@ -77,7 +77,7 @@ const Login = () => {
 
     return (
         <section className={styles.section} >
-                <a href="/"><img className={styles.coin} src={coin} alt="coin"/></a> 
+                <a href="/"><img className={styles.coin} src={worker} alt="coin"/></a> 
                 <br/>
                 <input 
                     className={styles.inputForm} 
