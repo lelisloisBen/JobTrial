5aec402d68bba2588acb4240aa5df4211f1e7d03
samir benzada <samirbenzada@gmail.com>
2020-02-22 23:34:22 -0500

    -m

--- src/js/components/Navbar/Navbar.js
+++ src/js/components/Navbar/Navbar.js
@@ -15,16 +15,16 @@ const IwashNavBar = () => {
         <nav className={["navbar fixed-top navbar-expand-lg navbar-light", styles.navBarI].join(' ')}>
             <div className="container">
                 <ul className="nav navbar-nav navbar-left mobile">
-                    <Link to="/">
+                    <Link className={styles.links} to="/">
                         HOME
                     </Link>
-                    <Link to="/">
+                    <Link className={styles.links} to="/">
                         ABOUT
                     </Link>
-                    <Link to="/">
+                    <Link className={styles.links} to="/">
                         SERVICE
                     </Link>
-                    <Link to="/">
+                    <Link className={styles.links} to="/">
                         CONTACT
                     </Link>
                 </ul>
