753286056996a56c3e0c8e16a73ae6bc46c88a10
gabsrodriguez <gmederos95@gmail.com>
2020-02-26 03:40:29 +0000

    sfd

--- src/js/views/Home/Home.js
+++ src/js/views/Home/Home.js
@@ -35,9 +35,18 @@ const Home = () => {
                 <div className="row" style={{minHeight: windowHeight}}>
                     <div ClassName="col-6" style={{width: "655px"}}>
                         <h2 className={styles.aboutUsHeading}>About Us</h2>
+                        <div className="container" style={{width:"500px"}}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Fames ac turpis egestas integer eget aliquet. Mi proin sed libero enim sed faucibus turpis in. Placerat in egestas erat imperdiet 
+                            sed euismod nisi. Eget nulla facilisi etiam dignissim. Et molestie ac feugiat sed lectus. Dolor sit amet consectetur adipiscing elit pellentesque. 
+                            Et ultrices neque ornare aenean euismod elementum. Tellus molestie nunc non blandit. Et odio pellentesque diam volutpat commodo sed egestas. 
+                            Placerat duis ultricies lacus sed turpis tincidunt id. Facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris. Purus non enim praesent 
+                            elementum facilisis leo vel. Mi proin sed libero enim sed faucibus turpis in. Amet est placerat in egestas.
+                        </div>
+                        <button className={styles.aboutUsButton}>Read More</button>
                     </div>
                     <div ClassName={styles.aboutUsGraphicDiv} style={{width: "655px", backgroundColor: "#588B8B"}}>
-                        <p>graphic goes here</p>
+                
                     </div>
                 
                 </div>
