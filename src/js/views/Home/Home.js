785185568ef06a091e126bfa29974817713d415a
gabsrodriguez <gmederos95@gmail.com>
2020-02-26 03:24:36 +0000

    slide two

--- src/js/views/Home/Home.js
+++ src/js/views/Home/Home.js
@@ -32,7 +32,15 @@ const Home = () => {
         <section className={styles.section2}>
 
             <div className={["text-center", styles.main].join(' ')} style={{minHeight: windowHeight}}>
+                <div className="row" style={{minHeight: windowHeight}}>
+                    <div ClassName="col-6" style={{width: "655px"}}>
+                        <h2 className={styles.aboutUsHeading}>About Us</h2>
+                    </div>
+                    <div ClassName={styles.aboutUsGraphicDiv} style={{width: "655px", backgroundColor: "#588B8B"}}>
+                        <p>graphic goes here</p>
+                    </div>
                 
+                </div>
             </div>
 
         </section>
