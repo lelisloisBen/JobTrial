9bffd329c87be436315617177550f8286f961452
samir benzada <samirbenzada@gmail.com>
2020-03-14 18:35:55 -0400

    -m

---
+++ src/js/views/Company/Company.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Company.module.css';
+
+const Company = () => {
+    return (
+        <section className={styles.section}>
+            company page
+
+        </section>
+    )
+}
+
+export default Company;
